bd7ec68274ff6ec6a899b4a8274ac374585df34a
Igor <borys6796@gmail.com>
2021-03-17 20:31:00 +0200

    Fix package.json, update view task for gulp

--- gulp-tasks/views.js
+++ gulp-tasks/views.js
@@ -5,20 +5,28 @@ import { task, src, dest } from "gulp";
 import include from "gulp-file-include";
 import gulpif from "gulp-if";
 import replace from "gulp-replace";
+import rename from "gulp-rename";
 import browsersync from "browser-sync";
 import yargs from "yargs";
 
 const argv = yargs.argv,
-    production = !!argv.production;
+	production = !!argv.production;
 
 task("views", () => {
-    return src(paths.views.src)
-        .pipe(include({
-            prefix: "@@",
-            basepath: "@file"
-        }))
-        .pipe(gulpif(production, replace(".css", ".min.css")))
-        .pipe(gulpif(production, replace(".js", ".min.js")))
-        .pipe(dest(paths.views.dist))
-        .pipe(browsersync.stream());
+	return src(paths.views.src)
+		.pipe(
+			include({
+				prefix: "@@",
+				basepath: "@file",
+			})
+		)
+		.pipe(
+			rename({
+				dirname: "",
+			})
+		)
+		.pipe(gulpif(production, replace(".css", ".min.css")))
+		.pipe(gulpif(production, replace(".js", ".min.js")))
+		.pipe(dest(paths.views.dist))
+		.pipe(browsersync.stream());
 });
